df03a05967b48450eb8396f2bbab630ae177e0ee
Mikhail <misha.verbi@gmail.com>
2018-12-08 23:48:55 +0300

    Added first version

---
+++ AllowedApplicationsMonitor/EvaluatorProcess.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllowedApplicationsMonitor
+{
+    class EvaluatorProcess
+    {
+        [DllImport("Rstrtmgr.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+        public static extern UInt32 RmStartSession(out UInt32 pSessionHandle, UInt32 dwSessionFlags,
+            string strSessionKey);
+
+        [DllImport("Rstrtmgr.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+        public static extern UInt32 RmRegisterResources(UInt32 dwSessionHandle,
+        UInt32 nFiles, string[] rgsFilenames, UInt32 nApplications,
+        ref RM_UNIQUE_PROCESS rgApplications, UInt32 nServices, string[] rgsServiceNames);
+
+        [DllImport("Rstrtmgr.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+        public static extern UInt32 RmGetList(UInt32 dwSessionHandle, out UInt32 pnProcInfoNeeded,
+        ref UInt32 pnProcInfo, [In, Out] RM_PROCESS_INFO[] rgAffectedApps, ref UInt32 lpdwRebootReasons);
+
+        [DllImport("Rstrtmgr.dll", CharSet = CharSet.Unicode, PreserveSig = true, SetLastError = true, ExactSpelling = true)]
+        public static extern UInt32 RmEndSession(UInt32 dwSessionHandle);
+
+        public const UInt32 RmRebootReasonNone = 0x0;
+        public const int ERROR_MORE_DATA = 234;
+    
+        public static System.Runtime.InteropServices.ComTypes.FILETIME FileTimeFromDateTime(DateTime date)
+        {
+            long ftime = date.ToFileTime();
+            System.Runtime.InteropServices.ComTypes.FILETIME ft = new System.Runtime.InteropServices.ComTypes.FILETIME();
+            ft.dwHighDateTime = (int)(ftime >> 32);
+            ft.dwLowDateTime = (int)ftime;
+            return ft;
+        }
+    
+        public static RM_APP_TYPE GetProcessType(Process proc)
+        {
+            uint handle;
+            string key = Guid.NewGuid().ToString();
+
+            uint res = RmStartSession(out handle, (uint)0, key);
+            if (res != 0)
+            {
+                throw new ApplicationException("Could not begin restart session. ");
+            }
+
+            try
+            {
+                uint pnProcInfoNeeded = 0, pnProcInfo = 0,
+                    lpdwRebootReasons = RmRebootReasonNone;
+
+                RM_UNIQUE_PROCESS uniqueprocess = new RM_UNIQUE_PROCESS();
+                uniqueprocess.dwProcessId = proc.Id;
+                System.Runtime.InteropServices.ComTypes.FILETIME ft = FileTimeFromDateTime(proc.StartTime);
+                uniqueprocess.ProcessStartTime = ft;
+
+                res = RmRegisterResources(handle, 0, null, 1, ref uniqueprocess, 0, null);
+
+                if (res != 0)
+                {
+                    throw new ApplicationException("Could not register resource.");
+                }
+
+                res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, null,
+                                ref lpdwRebootReasons);
+                if (res == ERROR_MORE_DATA)
+                {
+                    RM_PROCESS_INFO[] processInfo = new RM_PROCESS_INFO[pnProcInfoNeeded];
+                    pnProcInfo = pnProcInfoNeeded;
+
+                    res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo,
+                        processInfo, ref lpdwRebootReasons);
+                    if (res == 0)
+                    {
+                        if (pnProcInfo == 0) throw new ApplicationException("Process not found");
+
+                        return processInfo[0].ApplicationType;
+                    }
+                    else
+                    {
+                        throw new ApplicationException("Could not list processes");
+                    }
+                }
+                else if (res != 0)
+                {
+                    throw new ApplicationException("Failed to get size of result.");
+                }
+            }
+            finally
+            {
+                RmEndSession(handle);
+            }
+            throw new ApplicationException("Process not found");
+        }
+    }
+    
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RM_UNIQUE_PROCESS
+    {
+        public int dwProcessId;
+        public System.Runtime.InteropServices.ComTypes.FILETIME ProcessStartTime;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct RM_PROCESS_INFO
+    {
+        const int CCH_RM_MAX_APP_NAME = 255;
+        const int CCH_RM_MAX_SVC_NAME = 63;
+
+        public RM_UNIQUE_PROCESS Process;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_APP_NAME + 1)]
+        public string strAppName;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_SVC_NAME + 1)]
+        public string strServiceShortName;
+        public RM_APP_TYPE ApplicationType;
+        public uint AppStatus;
+        public uint TSSessionId;
+        [MarshalAs(UnmanagedType.Bool)]
+        public bool bRestartable;
+    }
+
+    public enum RM_APP_TYPE
+    {
+        // The application cannot be classified as any other type. 
+        RmUnknownApp = 0,
+        // A Windows application run as a stand-alone process that 
+        // displays a top-level window. 
+        RmMainWindow = 1,
+        // A Windows application that does not run as a stand-alone 
+        // process and does not display a top-level window. 
+        RmOtherWindow = 2,
+        // The application is a Windows service. 
+        RmService = 3,
+        // The application is Windows Explorer. 
+        RmExplorer = 4,
+        // The application is a stand-alone console application. 
+        RmConsole = 5,
+        // A system restart is required to complete the installation because 
+        // a process cannot be shut down. 
+        RmCritical = 1000
+    }
+}
---
+++ AllowedApplicationsMonitor/MainWindow.xaml.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Management;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AllowedApplicationsMonitor
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Привязывает переменную AllProcessesList
+        /// </summary>
+        private List<Process> allProcessesList;
+        public List<Process> AllProcessesList {
+            get { return allProcessesList; }
+            set
+            {
+                allProcessesList = value;
+                OnPropertyChanged("AllProcessesList");
+            }
+        }
+
+        private List<Process> allowedProcessesList;
+        public List<Process> AllowedProcessesList {
+            get { return allowedProcessesList; }
+            set
+            {
+                allowedProcessesList = value;
+                OnPropertyChanged("AllowedProcessesList");
+            }
+        }
+
+        private List<string> logsList;
+        public List<string> LogsList
+        {
+            get { return logsList; }
+            set
+            {
+                logsList = value;
+                OnPropertyChanged("LogsList");
+            }
+        }
+
+        ManagementEventWatcher startWatch;
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+
+            AllProcessesList = Process.GetProcesses().OrderBy(a => a.ProcessName).ToList();
+            AllowedProcessesList = AllProcessesList;
+            AllowedProcessesList.Add(Process.GetCurrentProcess());
+            LogsList = new List<string>();
+
+            startWatch = new ManagementEventWatcher(new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            startWatch.EventArrived += StartProcess;
+            startWatch.Start(); // всегда запускать от имени администратора
+
+            Closed += Dispose;
+        }
+        public void Dispose(object sender, EventArgs e)
+        {
+            startWatch.Stop();
+        }
+        public void StartProcess(object sender, EventArrivedEventArgs e)
+        {
+            try
+            {
+                Process process = Process.GetProcessById(Convert.ToInt32(e.NewEvent.Properties["ProcessID"].Value.ToString()));
+                KillProcess(process);
+            }
+            catch { }
+        }
+        public void MoveAllToAllowed(object sender, EventArgs e)
+        {
+            IEnumerable<Process> selected = AllList.SelectedItems.Cast<Process>();
+            AllowedProcessesList = AllowedProcessesList.Concat(selected).ToList();
+        }
+        public void MoveAllowedToAll(object sender, EventArgs e)
+        {
+            IEnumerable<Process> selected = AllowedList.SelectedItems.Cast<Process>().ToArray();
+            AllowedProcessesList = AllowedProcessesList.Except(selected).ToList();
+            
+            KillProcess(selected.ToArray());
+        }
+
+        private void KillProcess(params Process[] processes)
+        {
+            foreach (var item in processes)
+            {
+                var process = Process.GetProcessesByName(item.ProcessName).Single();
+                try
+                {
+                    if (process != null && !AllowedProcessesList.Select(a => a.ProcessName).Contains(process.ProcessName))
+                    {
+                        var assessment = EvaluatorProcess.GetProcessType(process);
+                        if (assessment == RM_APP_TYPE.RmExplorer
+                            || assessment == RM_APP_TYPE.RmMainWindow
+                            || assessment == RM_APP_TYPE.RmConsole
+                            || assessment == RM_APP_TYPE.RmUnknownApp
+                            || assessment == RM_APP_TYPE.RmOtherWindow)
+                        {
+                            process.Kill();
+
+                            LogsList = LogsList.Concat(new List<string>() { $"Закрыт: {process.ProcessName} - {DateTime.Now}" }).ToList();
+                        }
+                        else
+                        {
+                            AllProcessesList = AllProcessesList.Concat(new List<Process>() { process }).ToList();
+                        }
+                    }
+                }
+                catch{ }
+            }
+        }
+
+        /// <summary>
+        /// фигня для wpf чтобы при изменении значения переменной в этом классе менялось значение на форме
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
